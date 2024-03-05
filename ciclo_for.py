049e59b654504e678f85a2473894636266bdbab1
Andres420BL <158522084+Andres420BL@users.noreply.github.com>
2024-03-05 17:47:53 -0500

    Add files via upload

---
+++ ciclo_for.py
@@ -0,0 +1,8 @@
+z = 1
+sumatoria = 0
+cantidadint = int(input('Cantidad de lecturas------------->'))
+while  z <= cantidadint :
+    sumatoria += int(cantidadint+ 100000000 * (z * 10000))
+    print(sumatoria)
+    z+= 1 
+       
